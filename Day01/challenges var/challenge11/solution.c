f68c84c430c77bcb5ce169d5749b305addfdff8e
Kholoud <khoulodanfor@gmail.com>
2024-09-25 20:13:59 +0200

    challenge F

--- Day01/challenges var/challenge11/solution.c
+++ Day01/challenges var/challenge11/solution.c
@@ -1,8 +1,14 @@
-Surface = longueur * largeur
-    float longueur,largeur,surface;
+#include <stdio.h>
+int main (){
+
+
+
+ float longueur,largeur,surface;
+surface = longueur * largeur ;
+
     printf("entrez la longueur et la largeur du rectangle:");
     scanf("%f %f",&longueur,&largeur);
-    Surface = longueur * largeur
+    surface = longueur * largeur ;
     printf("la surface du rectangle =%f",surface);
      return 0;
 }
